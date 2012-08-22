e690874cbb963fa21f02f74a9af051627be51757
Chang Shin <aproxacs@gmail.com>
2012-08-22 16:55:50 +0900

    Updated spec_helper.rb and added .rspec for convenience.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,9 @@ Encoding.default_external = Encoding::UTF_8 if defined?(Encoding)
 Dir.glob(File.join(File.dirname(__FILE__), 'lib', 'shared_behaviors', '**', '.', '*.rb')).each do |f|
     require f
 end
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+end
